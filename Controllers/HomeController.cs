348924cbb388fc8476c1359fcfaf664a8491d403
Deborah <deborah.proen@gmail.com>
2020-11-13 09:39:27 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TesteFabio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Formulario()
+        {
+           
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var model = new TesteFabio.Models.Formulario();
+            //model.Nome = "Deborah";
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Salvar(TesteFabio.Models.Formulario model)
+        {
+            //model.Nome = "Deborah";
+            return Redirect("Index");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Sobre: ";
+            
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TesteFabio.MvcApplication" Language="C#" %>
